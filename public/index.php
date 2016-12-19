d52ff704e5e3448c15ffe740e78896292316c4b0
vanessasoutoc <vanessasoutoc@gmail.com>
2016-12-19 11:30:29 -0200

    erro openshift removido

--- public/index.php
+++ public/index.php
@@ -47,10 +47,9 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 |
 */
 
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel);
-/* php 7.0
+
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-*/
+
 
 $response = $kernel->handle(
     $request = Illuminate\Http\Request::capture()
