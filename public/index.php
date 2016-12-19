d9acf60308e811a2b353fe57b57fbfb92c999b33
vanessasoutoc <vanessasoutoc@gmail.com>
2016-12-19 10:37:01 -0200

    error in openshift line 50 in public/index.blade.php

--- public/index.php
+++ public/index.php
@@ -47,7 +47,10 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 |
 */
 
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel);
+/* php 7.0
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+*/
 
 $response = $kernel->handle(
     $request = Illuminate\Http\Request::capture()
